96cd97f41430bc22c2a762fb2a71103bf61a77df
dmytromaimesko <dima.maimesko@gmail.com>
2024-05-04 00:35:13 +0300

    Refactor MainController to use Twig and add additional dependencies
    The MainController has been refactored to extend AbstractController and to use the Twig template engine for rendering views. The rendering function has been updated to pass a 'starships' variable to the view. In addition, new dependencies have been added for Twig integration and their configurations have been adjusted. Some updates to the IDE settings have been made as well.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Hello World! Hello <strong>Dima Maimesko!</strong>');
+        $starships = 635;
+        return $this->render('main/homepage.html.twig', [
+            'starships' => $starships,
+        ]);
     }
 }
